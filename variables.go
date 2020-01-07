f688555bcb1cf1f9df6b22a9554ade804c29f841
yashkumaragrawal <30983273+yashkumaragrawal@users.noreply.github.com>
2020-01-07 17:12:10 +0530

    Create variables.go

---
+++ variables.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+
+    var a = "in"
+    fmt.Println(a)
+
+    var b, c int = 1, 2
+    fmt.Println(b, c)
+
+    var d = true
+    fmt.Println(d)
+
+    var e int
+    fmt.Println(e)
+
+    f := "apple"
+    fmt.Println(f)
+}
